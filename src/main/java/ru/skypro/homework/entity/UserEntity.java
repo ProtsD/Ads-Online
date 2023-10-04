008d2923972ff5e30c1711fb7bc12f5e30bd12ad
Baggovut <117596144+Baggovut@users.noreply.github.com>
2023-10-04 15:22:04 +0300

    Additional small fixes

--- src/main/java/ru/skypro/homework/entity/UserEntity.java
+++ src/main/java/ru/skypro/homework/entity/UserEntity.java
@@ -14,17 +14,17 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
     @Column(name = "password", nullable = false)
     private String password;
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
     @Column(name = "first_name", nullable = false)
     private String firstName;
     @Column(name = "last_name", nullable = false)
     private String lastName;
-    @Column(name = "phone", nullable = false)
+    @Column(name = "phone", nullable = false, unique = true)
     private String phone;
     @Column(name = "role", nullable = false)
     private Role role;
