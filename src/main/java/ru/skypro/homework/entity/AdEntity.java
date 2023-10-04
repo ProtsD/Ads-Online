3ad74af25cb91634717eca1cb2852f5d4bce41db
Baggovut <117596144+Baggovut@users.noreply.github.com>
2023-10-04 14:06:27 +0300

    Small fixes

--- src/main/java/ru/skypro/homework/entity/AdEntity.java
+++ src/main/java/ru/skypro/homework/entity/AdEntity.java
@@ -4,9 +4,6 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -19,15 +16,12 @@ public class AdEntity {
     private Integer pk;
 
     @Column(name = "price", nullable = false)
-    @Min(0) @Max(10000000)
     private Integer price;
 
     @Column(name = "title", nullable = false)
-    @Size(min = 4, max = 32)
     private String title;
 
     @Column(name = "description", nullable = false)
-    @Size(min = 8, max = 64)
     private String description;
 
     @Column(name = "image", nullable = false)
