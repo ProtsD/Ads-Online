962c8ed8ba09468a64ca05c29740d7e838620762
Handsken <nan.222@yandex.ru>
2023-10-07 20:10:00 +0500

    fix commentservice

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -15,30 +15,31 @@ import ru.skypro.homework.service.CommentService;
 
 
 @RestController
+@RequestMapping("/ads")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
-    @GetMapping("ads/{id}/comments")
+    @GetMapping("/{id}/comments")
     public ResponseEntity<Comments> getAllCommentsForAd(Authentication authentication,@PathVariable int id){
         Comments comments =commentService.getAllComments(authentication,id);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(comments);
     }
-    @PostMapping("ads/{id}/comments")
+    @PostMapping("/{id}/comments")
     public ResponseEntity<Comment> createComment(Authentication authentication, @PathVariable int id, @RequestBody CreateOrUpdateComment createOrUpdateComment){
         Comment comment =commentService.createComment(authentication,id,createOrUpdateComment);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(comment);
     }
-    @DeleteMapping("ads/{adId}/comments/{commentId}")
+    @DeleteMapping("/{adId}/comments/{commentId}")
     public ResponseEntity<?> deleteComment(Authentication authentication,@PathVariable(name = "adId") int adId, @PathVariable(name = "commentId") int commentId){
         commentService.deleteComment(authentication,adId, commentId);
         return ResponseEntity.ok().build();
     }
-    @PatchMapping("ads/{adId}/comments/{commentId}")
+    @PatchMapping("/{adId}/comments/{commentId}")
     public ResponseEntity<Comment> updateComment(Authentication authentication,@PathVariable(name = "adId") int adId, @PathVariable(name = "commentId") int commentId, @RequestBody @Valid CreateOrUpdateComment createOrUpdateComment){
         Comment comment =commentService.updateComment(authentication,adId,commentId,createOrUpdateComment);
         return ResponseEntity.ok()
--- src/main/java/ru/skypro/homework/service/impl/CommentServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentServiceImpl.java
@@ -1,18 +1,22 @@
 package ru.skypro.homework.service.impl;
 
+import liquibase.pro.packaged.A;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.comment.Comment;
 import ru.skypro.homework.dto.comment.Comments;
 import ru.skypro.homework.dto.comment.CreateOrUpdateComment;
+import ru.skypro.homework.dto.user.User;
 import ru.skypro.homework.entity.CommentEntity;
 import ru.skypro.homework.entity.UserEntity;
+import ru.skypro.homework.exception.ForbiddenException;
 import ru.skypro.homework.mapper.CommentMapper;
 import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.repository.AdRepository;
 import ru.skypro.homework.repository.CommentRepository;
 import ru.skypro.homework.repository.UserRepository;
+import ru.skypro.homework.security.SecurityUserPrincipal;
 import ru.skypro.homework.service.CommentService;
 
 import java.util.stream.Collectors;
@@ -35,17 +39,15 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public Comment createComment(Authentication authentication, int id, CreateOrUpdateComment createOrUpdateComment) {
-        CommentEntity result = commentMapper.toNewEntity(createOrUpdateComment,userRepository.findByUsername(authentication.getName()).orElseThrow(),adRepository.findById(id).orElseThrow());
+        CommentEntity result = commentMapper.toNewEntity(createOrUpdateComment,userRepository.findById(getCurrentUser(authentication).getId()).orElseThrow(),adRepository.findById(id).orElseThrow());
         result=commentRepository.save(result);
         return commentMapper.toComment(result);
     }
 
     @Override
     public void deleteComment(Authentication authentication, int adId, int commentId) {
-        String authenticationName = authentication.getName();
-        UserEntity userEntity = userRepository.findByUsername(authenticationName).orElseThrow();
         CommentEntity commentEntity = commentRepository.findById(commentId).orElseThrow();
-        if (userEntity.getUsername()==commentEntity.getAuthor().getUsername() & commentEntity.getAdsEntity().getPk()==adId){
+        if (checkUser(authentication,adId,commentEntity)){
             commentRepository.deleteById(commentId);
         }
         else {
@@ -54,16 +56,28 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public Comment updateComment(Authentication authentication, int adId, int commentId, CreateOrUpdateComment createOrUpdateComment) {
-        String authenticationName = authentication.getName();
-        UserEntity userEntity = userRepository.findByUsername(authenticationName).orElseThrow();
         CommentEntity commentEntity = commentRepository.findById(commentId).orElseThrow();
-        if (userEntity.getUsername()==commentEntity.getAuthor().getUsername() & commentEntity.getAdsEntity().getPk()==adId){
+        if (checkUser(authentication,adId,commentEntity)){
             commentEntity.setText(createOrUpdateComment.getText());
             Comment result = commentMapper.toComment(commentRepository.save(commentEntity));
             return result;
         }
-        else {
+        else{
             return null;
         }
+    }
+    private boolean checkUser(Authentication authentication,int adId,CommentEntity commentEntity){
+        if (getCurrentUser(authentication).getId()==commentEntity.getAuthor().getId() & commentEntity.getAdsEntity().getPk()==adId){
+            return true;
+        }
+        else if (commentEntity.getAdsEntity().getPk()==adId) {
+            return false;
+        }
+        else {
+            throw new ForbiddenException("");
+        }
+    }
+    private User getCurrentUser(Authentication authentication) {
+        return ((SecurityUserPrincipal) authentication.getPrincipal()).getUserDto();
     }
 }
