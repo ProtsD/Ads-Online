989461defb1407b10e1daa6efede7f0ff113a488
Baggovut <117596144+Baggovut@users.noreply.github.com>
2023-10-09 05:40:42 +0300

    Fix for user avatar image.

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package ru.skypro.homework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -41,6 +43,31 @@ public class WebSecurityConfig {
                 .and()
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    /*
+     * у фронта в данном методе у переменной password значение уже зашифрованное приходит,
+     * из-за этого GET-запрос постоянно выдаёт 401 ошибку, когда во время авторизации он повторно шифрует уже зашифрованный пароль и логин.
+     *
+     *     getUserPhoto(imageId, username, password) {
+     *         return fetch(`${this._url}${imageId}`, {
+     *             headers: {
+     *                 method: 'GET',
+     *                 Authorization: "Basic " + base64.encode(`${username}:${password}`),
+     *             },
+     *         }).then(res => {
+     *             if (!res.ok) {
+     *                 return Promise.reject(`Error: ${res.status}`)
+     *             }
+     *
+     *             return res.blob();
+     *         });
+     *     }
+     * Как без данного костыля проблему решить я не знаю.
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().mvcMatchers(HttpMethod.GET, "/images/**");
     }
 
     @Bean
