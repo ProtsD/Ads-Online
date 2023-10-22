a1ed7b51efaab24bcf14b3969deb51d4053d4a7d
Baggovut <117596144+Baggovut@users.noreply.github.com>
2023-10-22 15:53:29 +0300

    ImageService javadoc.

--- src/main/java/ru/skypro/homework/service/ImageService.java
+++ src/main/java/ru/skypro/homework/service/ImageService.java
@@ -4,11 +4,36 @@ import ru.skypro.homework.entity.ImageEntity;
 
 public interface ImageService {
     String IMAGE_URL_PREFIX = "/images/";
+
+    /**
+     * Returns ImageEntity of the image with the given id.
+     *
+     * @param id requested image ID
+     * @return ImageEntity of requested image
+     */
     ImageEntity getImage(Integer id);
 
+    /**
+     * Saves a new image
+     *
+     * @param image image bytes
+     * @return ImageEntity of requested image
+     */
     ImageEntity uploadImage(byte[] image);
 
+    /**
+     * Updates existed image with the given id.
+     *
+     * @param id    updated image ID
+     * @param image image bytes
+     * @return ImageEntity of requested image
+     */
     ImageEntity updateImage(Integer id, byte[] image);
 
+    /**
+     * Deletes the image with the given id.
+     *
+     * @param id deleted image ID
+     */
     void deleteImage(Integer id);
 }
